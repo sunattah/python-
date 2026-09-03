3e7243b9cb7f6cceae65dca0216ad0cd6429df8e
sunattah <sundayattah319@gmail.com>
2026-09-03 11:44:37 +0100

    my guessing game

--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -1,11 +1,20 @@
-def get_guessing():
+import random
 
-   guess_number = input("guess a number: ")
-   while True:
-    try:
-      to_int = int(guess_number)
-      if guess_number:
+def get_guessing():
+    x = 1
+    y = 100
+    select_number = random.randint(x, y)
+    for number_range in select_number:
+       guess_number = input("guess a number: ")
+       if guess_number  not in select_number:
+        print("not the correct number")
+
+       while True:
+        try:
+         to_int = int(guess_number)
+         if guess_number:
           return True
-    except ValueError:
+        except ValueError:
          print("invalid number")
+         break
 get_guessing()
