23636cbd9b607ca3979eef6329551bbe0525d330
sunattah <sundayattah319@gmail.com>
2026-09-03 02:45:30 +0100

    my number guessing game

--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -1,6 +1,11 @@
-guess_number = input("guess a number: ")
-while True:
-try:
-    to_int = int(guess_number)
-except ValueError:
-        print("invalid number")
+def get_guessing():
+
+   guess_number = input("guess a number: ")
+   while True:
+    try:
+      to_int = int(guess_number)
+      if guess_number:
+          return True
+    except ValueError:
+         print("invalid number")
+get_guessing()
