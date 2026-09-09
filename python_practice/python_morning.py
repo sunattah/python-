d4ec800d3683265eef1795a382cb7152dc4adc1a
sunattah <sundayattah319@gmail.com>
2026-09-09 11:07:43 +0100

    one of talent nation lessons

--- python_practice/python_morning.py
+++ python_practice/python_morning.py
@@ -1,4 +1,11 @@
-balance = 7000
-balance = 7000
-print(balance - balance)
+# balance = 7000
+# balance = 7000
+# print(balance * balance)
 
+def morning_drill():
+    first_thing = "brush my teeth"
+    second_thing = "take my bath"
+    third_thing = "take my breakfast"
+    forth_thing = "leave"
+    print(f"In the morning when i wake up i {first_thing}, and {second_thing}, then {third_thing}, and {forth_thing}")
+morning_drill()
