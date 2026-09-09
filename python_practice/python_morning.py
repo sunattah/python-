3101a8e1d99c906cd975e518760b66cad9a85296
sunattah <sundayattah319@gmail.com>
2026-09-09 11:26:03 +0100

    morning drill been done

--- python_practice/python_morning.py
+++ python_practice/python_morning.py
@@ -3,9 +3,15 @@
 # print(balance * balance)
 
 def morning_drill():
-    first_thing = "brush my teeth"
-    second_thing = "take my bath"
-    third_thing = "take my breakfast"
-    forth_thing = "leave"
-    print(f"In the morning when i wake up i {first_thing}, and {second_thing}, then {third_thing}, and {forth_thing}")
+    first_thing = "1 brush my teeth"
+    second_thing = "2 take my bath"
+    third_thing = "3 take my breakfast"
+    forth_thing = "4 leave"
+    print(f"In the morning when i wake up i: {first_thing}, and, {second_thing}, then, {third_thing}, and {forth_thing}")
 morning_drill()
+def done():
+    first_thing = "brush my teeth"
+
+    num1 = 1
+    print(1, first_thing)
+done()
