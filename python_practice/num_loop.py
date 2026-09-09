82c2a6a3127680834a12f59b38cdf68f6aea1122
sunattah <sundayattah319@gmail.com>
2026-09-09 21:10:28 +0100

    looping through python is a way hoo

--- python_practice/num_loop.py
+++ python_practice/num_loop.py
@@ -1,7 +1,9 @@
 # number =  [9,0,8,7,6,6,5,2,5,1]
 # print(sorted(number))
 
-balance = 5000
-print("balance before withdrawal", balance)
-after_withdrawal = balance - 1000
-print("after withdrawal", after_withdrawal)
+# balance = 5000
+# print("balance before withdrawal", balance)
+# after_withdrawal = balance - 1000
+# print("after withdrawal", after_withdrawal)
+for num in [1,2,4,5,6,78,9]:
+    print(num)
