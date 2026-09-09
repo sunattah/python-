e0c8d46a181c11757e2e7bcb39217dec75734b3d
sunattah <sundayattah319@gmail.com>
2026-09-09 14:30:20 +0100

    the looping integers

--- python_practice/num_loop.py
+++ python_practice/num_loop.py
@@ -1,2 +1,7 @@
-number =  [9,0,8,7,6,6,5,2,5,1]
-print(sorted(number))
+# number =  [9,0,8,7,6,6,5,2,5,1]
+# print(sorted(number))
+
+balance = 5000
+print("balance before withdrawal", balance)
+after_withdrawal = balance - 1000
+print("after withdrawal", after_withdrawal)
