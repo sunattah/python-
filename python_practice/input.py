5a8f350e3b2c2a18fd25e90d220f58b5b24b8fcc
sunattah <sundayattah319@gmail.com>
2026-09-10 11:32:24 +0100

    input number file

--- python_practice/input.py
+++ python_practice/input.py
@@ -1,4 +1,7 @@
-enter_number = int(input("enter your specific number: "))
 def add(number1, number2):
+   
    return number1 + number2
+enter_number = int(input("enter your specific number: "))
+if enter_number is not int(input("enter your specific number: ")):
+   print("input the valid number not letters")
 print(add(enter_number, 4 ))
