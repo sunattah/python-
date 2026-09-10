25309ccf89af3643090c26da209e55ef229768da
sunattah <sundayattah319@gmail.com>
2026-09-10 11:21:23 +0100

    input number file

--- python_practice/input.py
+++ python_practice/input.py
@@ -1,2 +1,4 @@
-enter_number = input("enter your specific number: ")
-print(enter_number)
+enter_number = int(input("enter your specific number: "))
+def add(number1, number2):
+   return number1 + number2
+print(add(enter_number, 4 ))
