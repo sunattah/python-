c05fc767ecee965f603da2f35f5d40e067a482dc
sunattah <sundayattah319@gmail.com>
2026-09-14 12:09:44 +0100

    the guessing game

--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -1,77 +1,8 @@
-# # import random
-
-# def get_guessing():
-#     # x = 1
-#     # y = 100
-#     # select_number = random.randint(x, y)
-
-#     guess_number = input("guess a number: ")
-
-#     # if guess_number  not in select_number:
-#     #    print("not the correct number")
-
-#     while True:
-#       try:
-#         to_int = int(guess_number)
-#         if guess_number:
-#           return True
-#       except ValueError:
-#         print("invalid number")
-#         break
-# get_guessing()
-
-# .py
-import random
-
-# define range and max_attempts
-lower_bound = 1
-upper_bound = 1000
-max_attempts = 10
-
-# generate the secret number
-secret_number = random.randint(lower_bound, upper_bound)
-
-# Get the user's guess
-def get_guess():
+def guess_number():
     while True:
         try:
-            guess = int(input(f"Guess a number between {lower_bound} and {upper_bound}: "))
-            if lower_bound <= guess <= upper_bound:
-                return guess
-            else:
-                print("Invalid input. Please enter a number within the specified range.")
+            number_guessing = int(input("enter number: "))
         except ValueError:
-            print("Invalid input. Please enter a valid number.")
-
-# Validate guess
-def check_guess(guess, secret_number):
-    if guess == secret_number:
-        return "Correct"
-    elif guess < secret_number:
-        return "Too low"
-    else:
-        return "Too high"
-
-# track the number of attempts, detect if the game is over
-def play_game():
-    attempts = 0
-    won = False
-
-    while attempts < max_attempts:
-        attempts += 1
-        guess = get_guess()
-        result = check_guess(guess, secret_number)
-
-        if result == "Correct":
-            print(f"Congratulations! You guessed the secret number {secret_number} in {attempts} attempts.")
-            won = True
-            break
-        else:
-            print(f"{result}. Try again!")
-
-    if not won:
-        print(f"Sorry, you ran out of attempts! The secret number is {secret_number}.")
-
-if __name__ == "__main__":
-    print("Welcome to the Number Guessing Game!")
-    play_game()
+          print("invalid number")
+guess_number()
+              
