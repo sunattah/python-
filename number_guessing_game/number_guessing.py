e3a75f2302d57e48fea207b507d6702c34861103
sunattah <sundayattah319@gmail.com>
2026-09-14 14:43:47 +0100

    the guessing game

--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -1,8 +1,17 @@
+import random
 def guess_number():
     while True:
         try:
             number_guessing = int(input("enter number: "))
         except ValueError:
           print("invalid number")
-guess_number()
-              
+# guess_number
+def input_number():
+    x = 1
+    y = 100
+    guessing = random.randint(x, y)
+    if guess_number < guessing:
+        print("lesser number: try again")
+        if guess_number > guessing:
+            print("greater number: try again")
+input_number()
