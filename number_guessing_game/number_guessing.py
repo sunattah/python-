541e334d97150a733a3e030080136c4e1457e0a2
sunattah <sundayattah319@gmail.com>
2026-09-15 11:48:57 +0100

    the guessing game

--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -2,14 +2,15 @@ import random
 
 def guess_number(x, y):
     while True:
-          try:
+        try:
             to_int = int(input("enter number: "))
             if x <= to_int <= y:
                 return to_int
             else:
                 print(f"Out of range! Please enter a number between {x} and {y}.")
-          except ValueError:
+        except ValueError:
             print("invalid number")
+
 def input_number():
     x = 1
     y = 100
@@ -36,11 +37,12 @@ def input_number():
 
 def main_game_loop():
     while True:
-        
         input_number()
 
         play_again = input("\nplay again (y/n) ").strip().lower()
-        if play_again != "y" or play_again != "yes":
+        
+        if play_again not in ["y", "yes"]:
             print("thanks for playing and goodbye")
             break
+
 main_game_loop()
