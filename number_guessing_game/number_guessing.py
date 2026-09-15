0a3fb40e6e7b56f4fe6da09b745cc5909cadafe0
sunattah <sundayattah319@gmail.com>
2026-09-15 03:09:20 +0100

    the guessing game

--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -3,7 +3,6 @@
 def guess_number():
     while True:
         try:
-            # Added 'return' so the function actually gives the number back
             return int(input("enter number: "))
         except ValueError:
             print("invalid number")
@@ -14,17 +13,18 @@ def input_number():
     guessing = 42
     
     print(f"I am thinking of a number between {x} and {y}.")
-
+    counter = 0
     while True:
-        # MOVED INSIDE THE LOOP: This asks the user for a new guess every turn
         input_guess = guess_number()
         
         if input_guess < guessing:
+            counter += 1
             print("lesser number: try again")
         elif input_guess > guessing:
             print("greater number: try again")
+            counter += 1
         else:
-            print("You win: it is the correct number")
+            print(f"You win: it is the correct number in {counter}")
             break
 
 input_number()
