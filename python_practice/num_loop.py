781f54e269c28cf573f81f146b4855e28189377c
sunattah <sundayattah319@gmail.com>
2026-09-16 13:27:35 +0100

    my python loop

--- python_practice/num_loop.py
+++ python_practice/num_loop.py
@@ -1,9 +1,20 @@
-# number =  [9,0,8,7,6,6,5,2,5,1]
-# print(sorted(number))
+'''
+number =  [9,0,8,7,6,6,5,2,5,1]
+print(sorted(number))
 
-# balance = 5000
-# print("balance before withdrawal", balance)
-# after_withdrawal = balance - 1000
-# print("after withdrawal", after_withdrawal)
+balance = 5000
+print("balance before withdrawal", balance)
+after_withdrawal = balance - 1000
+print("after withdrawal", after_withdrawal)
 for num in [1,2,4,5,6,78,9]:
     print(num)
+'''
+y = "positive"
+x = "Negative"
+user_number = int(input("enter a number: ")) 
+if user_number < 0:
+    print(f"The number is {x}, it should be {y}")
+elif user_number > 0:
+    print("The number is positive")
+else:
+    print("The number is zero")
