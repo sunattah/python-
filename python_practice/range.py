2ba97937938a7d135fe56f2d4d518531cdbc4083
sunattah <sundayattah319@gmail.com>
2026-09-17 16:49:01 +0100

    ranging through list

--- python_practice/range.py
+++ python_practice/range.py
@@ -5,3 +5,16 @@ an_list = [1,32,4,5,6,7]
 for ch in an_list:
     for gh in range(6):
      print(f"first list = {ch} and second = {gh}")
+     break
+def num():
+    i = 3
+    while 30:
+       j = 7
+       if i // j == 0:
+          print("ok")
+          break
+          continue
+       if i % j == 0:
+          print("wrong")
+num()
+       
