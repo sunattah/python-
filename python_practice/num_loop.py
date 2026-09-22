7f36133246c37b07dd8e67406c75bfb4ab9239e6
sunattah <sundayattah319@gmail.com>
2026-09-22 14:29:22 +0100

    loop is running

--- python_practice/num_loop.py
+++ python_practice/num_loop.py
@@ -9,12 +9,21 @@ print("after withdrawal", after_withdrawal)
 for num in [1,2,4,5,6,78,9]:
     print(num)
 '''
-y = "positive"
-x = "Negative"
-user_number = int(input("enter a number: ")) 
-if user_number < 0:
-    print(f"The number is {x}, it should be {y}")
-elif user_number > 0:
-    print("The number is positive")
-else:
-    print("The number is zero")
+def know():
+    y = "positive"
+    x = "Negative"
+    user_number = int(input("enter a number: ")) 
+    if user_number < 0:
+        print(f"The number is {x}, it should be {y}")
+    elif user_number > 0:
+        print("The number is positive")
+    else:
+        print("The number is zero")
+know()
+
+def loop():
+     market_list = ["apple", "creyfish", "yam", "potatoes", "fish", "meat"]
+     for main_thing in market_list:
+         favorite = main_thing
+         print(favorite)
+loop()
