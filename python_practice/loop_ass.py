3c6e4ed99b2e14a76f1e13c94289a68fb326f4d4
sunattah <sundayattah319@gmail.com>
2026-09-22 17:13:46 +0100

    range of numbers

---
+++ python_practice/loop_ass.py
@@ -0,0 +1,9 @@
+# inventory_data = [10, "broken_id", 25, -5, 40, "missing", 55, 0, 70]
+
+# inventory_data = [10, "broken_id", 25, -5, 40, "missing", 55, 0, 70]
+
+# Task 1: The Multiples Loop 
+# Create a for loop using range() that prints out every multiple of 15 from 15 up to and including 90.
+# Fill in the start, stop, and step values
+for number in range(15, 91, 15):
+    print(number)
