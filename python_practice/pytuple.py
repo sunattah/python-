27dd3e8670bf3522277473c1707580f7cdacf21b
sunattah <sundayattah319@gmail.com>
2026-09-23 13:02:17 +0100

    tuple in python

--- python_practice/pytuple.py
+++ python_practice/pytuple.py
@@ -5,3 +5,6 @@ print(list_item)
 scores = (1500, 2200, 3100, 4500, 5000, 7200, 8800, 9900)
 three_item = scores[5:8]
 print(three_item)
+print(scores[1:6:2])
+reverse_score = scores[: : -1]
+print(reverse_score)
